bc95e4ceba6d7f7d5fc5d45e268af539abbd5d46
DM <opensource@hello.menu>
2024-05-19 19:17:44 +0100

    support .get()

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import { BehaviorSubject } from 'rxjs';
 export const ObservableSignal = <T>(initial: T) => {
 	const state = new BehaviorSubject<T>(initial);
 
-	const sink = <BehaviorSubject<T> & T>new Proxy(state, {
+	const sink = new Proxy(state, {
 		get(target, prop, receiver) {
-			return (
-				[Symbol.toPrimitive, 'valueOf', 'toString'].includes(prop)
-					? () => <T>state.value
-					: <T>Reflect.get(target, prop, receiver)
-			);
+			if([Symbol.toPrimitive, 'valueOf', 'toString', 'get'].includes(prop)) {
+				return () => state.value
+			} else {
+				return <T>Reflect.get(target, prop, receiver)
+			};
 		}
-	});
-
+	}) as T & BehaviorSubject<T> & { get: () => T };
 
 	const emitter = <(value: T) => void>state.next.bind(state);
 	return <const>[sink, emitter];
 };
+
